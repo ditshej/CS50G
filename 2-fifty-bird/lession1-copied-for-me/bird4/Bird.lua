3ed5d73dce1c9d918fe65eabb6e2a6ed0ca4320d
Raphi W <ditshej@gmail.com>
2020-05-30 17:36:28 +0200

    fifty-bird4

--- 2-fifty-bird/lession1-copied-for-me/bird4/Bird.lua
+++ 2-fifty-bird/lession1-copied-for-me/bird4/Bird.lua
@@ -1,4 +1,4 @@
-Bird = Class{}
+Bird = Class {}
 
 local GRAVITY = 20
 
@@ -7,14 +7,18 @@ function Bird:init()
     self.width = self.image:getWidth()
     self.height = self.image:getHeight()
 
-    self.x = VIRTUAL_WIDTH / 2 - ( self.width / 2 )
-    self.y = VIRTUAL_HEIGHT / 2 - ( self.height / 2 )
+    self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+    self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
 
     self.dy = 0
 end
 
 function Bird:update(dt)
     self.dy = self.dy + GRAVITY * dt
+
+    if love.keyboard.wasPressed('space') then
+        self.dy = -5
+    end
 
     self.y = self.y + self.dy
 end
