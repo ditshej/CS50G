fcf5cbb64bc9abe81889f77bd46e234cf210b391
Raphi W <ditshej@gmail.com>
2020-05-31 13:41:35 +0200

    fifty-bird6: reset main.lua, Bird.loa and Pipe.lua to bird5 and empty PipePair.lua

--- 2-fifty-bird/lession1-copied-for-me/bird6/Pipe.lua
+++ 2-fifty-bird/lession1-copied-for-me/bird6/Pipe.lua
@@ -1,42 +1,21 @@
---[[
-    Pipe Class
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    The Pipe class represents the pipes that randomly spawn in our game, which act as our primary obstacles.
-    The pipes can stick out a random distance from the top or bottom of the screen. When the player collides
-    with one of them, it's game over. Rather than our bird actually moving through the screen horizontally,
-    the pipes themselves scroll through the game to give the illusion of player movement.
-]]
-
 Pipe = Class{}
 
--- since we only want the image loaded once, not per instantation, define it externally
 local PIPE_IMAGE = love.graphics.newImage('pipe.png')
 
--- speed at which the pipe should scroll right to left
-PIPE_SPEED = 60
+local PIPE_SCROLL = -60
 
--- height of pipe image, globally accessible
-PIPE_HEIGHT = 288
-PIPE_WIDTH = 70
-
-function Pipe:init(orientation, y)
+function Pipe:init()
     self.x = VIRTUAL_WIDTH
-    self.y = y
+
+    self.y = math.random(VIRTUAL_HEIGHT / 4, VIRTUAL_HEIGHT - 10)
 
     self.width = PIPE_IMAGE:getWidth()
-    self.height = PIPE_HEIGHT
-
-    self.orientation = orientation
 end
 
 function Pipe:update(dt)
-    
+    self.x = self.x + PIPE_SCROLL * dt
 end
 
 function Pipe:render()
-    love.graphics.draw(PIPE_IMAGE, self.x, 
-        (self.orientation == 'top' and self.y + PIPE_HEIGHT or self.y), 
-        0, 1, self.orientation == 'top' and -1 or 1)
+    love.graphics.draw(PIPE_IMAGE, self.x, self.y)
 end
