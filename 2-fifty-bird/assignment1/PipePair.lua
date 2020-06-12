dba800b84671db70179ad8c7783389843e16443d
Raphi W <ditshej@gmail.com>
2020-06-12 11:51:56 +0200

    assignment1: implement random gab height

--- 2-fifty-bird/assignment1/PipePair.lua
+++ 2-fifty-bird/assignment1/PipePair.lua
@@ -6,12 +6,11 @@
 
     Used to represent a pair of pipes that stick together as they scroll, providing an opening
     for the player to jump through in order to score a point.
+
+    @update by ditshej: implement random GAB_HEIGHT for every pipe pair
 ]]
 
 PipePair = Class{}
-
--- size of the gap between pipes
-local GAP_HEIGHT = 90
 
 function PipePair:init(y)
     -- flag to hold whether this pair has been scored (jumped through)
@@ -23,10 +22,13 @@ function PipePair:init(y)
     -- y value is for the topmost pipe; gap is a vertical shift of the second lower pipe
     self.y = y
 
+    -- random size of the gap between pipes
+    self.gabHeight = math.random(90,150)
+
     -- instantiate two pipes that belong to this pair
     self.pipes = {
         ['upper'] = Pipe('top', self.y),
-        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + GAP_HEIGHT)
+        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + self.gabHeight)
     }
 
     -- whether this pipe pair is ready to be removed from the scene
